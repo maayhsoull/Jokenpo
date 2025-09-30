608e0a6179b4be0f3e0d7398c6cd86e9d088b12b
maayhsoull <maysenai2025@gmail.com>
2025-09-29 22:34:04 -0300

    Solicitando dados e inserindo random para obter dados aleatorios

--- src/Model/Jokenpo.java
+++ src/Model/Jokenpo.java
@@ -1,4 +1,31 @@
 package Model;
 
+import java.util.Scanner;
+
 public class Jokenpo {
+    int escolha;
+    int numeroAleatorio = (int) (Math.random() * 4);
+
+    public void testandoRandom(){
+        System.out.println(numeroAleatorio);
+    }
+
+    public void obterDados(){
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("--------------------------------------");
+        System.out.print("*** Vamos jogar Jokenpô! ***");
+        System.out.print("--------------------------------------");
+        System.out.println("Escolha uma opção: ");
+        System.out.print("(1) Pedra");
+        System.out.print("(2) Papel");
+        System.out.print("(3) Tesoura");
+        System.out.print("--------------------------------------");
+        System.out.print("Digite sua escolha: ");
+        escolha = leitor.nextInt();
+
+    }
+
+    public void jogar(){
+
+    }
 }
--- src/br/senai/sp/jandira/ds1m/jokenpo/JokenpoApp.java
+++ src/br/senai/sp/jandira/ds1m/jokenpo/JokenpoApp.java
@@ -1,4 +1,11 @@
 package br.senai.sp.jandira.ds1m.jokenpo;
 
+import Model.Jokenpo;
+
 public class JokenpoApp {
+    public static void main(String[] args) {
+        Jokenpo jokenpo = new Jokenpo();
+
+        jokenpo.testandoRandom();
+    }
 }
